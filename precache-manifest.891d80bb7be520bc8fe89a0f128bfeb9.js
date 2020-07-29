0192fb088f533fdab88e191f3815dcfc90a8f451
akay <aungkhantko@protonmail.com>
2020-07-29 01:04:15 -0700

    Minor edits to content and font change

---
+++ precache-manifest.891d80bb7be520bc8fe89a0f128bfeb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40868c9144127b32c7244eac2a9bb51c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfdfd34767d043d9dedc",
+    "url": "./static/css/main.e48f9436.chunk.css"
+  },
+  {
+    "revision": "88738a99267153528b1f",
+    "url": "./static/js/2.cbc61858.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.cbc61858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdfd34767d043d9dedc",
+    "url": "./static/js/main.6733b778.chunk.js"
+  },
+  {
+    "revision": "989208dbd57b89fd121e",
+    "url": "./static/js/runtime-main.796b5bb3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a481001f8a8441b3ae50cfec0c4566ae.js"
+  "./precache-manifest.891d80bb7be520bc8fe89a0f128bfeb9.js"
 );
 
 self.addEventListener('message', (event) => {
