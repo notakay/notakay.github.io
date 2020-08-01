ef253d5095014ccda7d9997af06e0cd4108b1997
akay <aungkhantko@protonmail.com>
2020-08-01 04:18:38 -0700

    Added pancake writeup

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d2debf050d9f70dcee22efff6ffb0d92.js"
+  "./precache-manifest.322c8ec41499fd4eb7630324ed63fc21.js"
 );
 
 self.addEventListener('message', (event) => {
